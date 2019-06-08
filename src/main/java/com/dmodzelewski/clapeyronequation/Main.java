bd806a9cb951d4773b208b05cdc514bf5bfa4171
carrpenoctem <dn.mski@gmail.com>
2019-06-08 11:21:19 +0200

    All variables can be calculated now

--- src/main/java/com/dmodzelewski/clapeyronequation/Main.java
+++ src/main/java/com/dmodzelewski/clapeyronequation/Main.java
@@ -12,10 +12,10 @@ import java.util.logging.Logger;
 public class Main {
 
     /* Pressure, Volume, number of Moles, Temperature and Gas constant */
-    private static float P;     // [hPa]
-    private static float V;     // [dm^3]
+    private static float P;     // [Pa]
+    private static float V;     // [m^3]
     private static float n;     // [mol]
-    private static float T;     //[K]
+    private static float T;     // [K]
     private static final float R = 8.314f; // [J/mol*K]
     /* Logger, for logging errors */
     private static final Logger classLogger = Logger.getLogger(Main.class.getCanonicalName());
@@ -28,19 +28,29 @@ public class Main {
             System.out.println("What shall be calculated? \n input: P,V,n or T");
             String selectedVariable = input.nextLine();
             if (selectedVariable.equalsIgnoreCase("p")) {
-                // calculate P
+                calculateP();
+                running = false;
             } else if (selectedVariable.equalsIgnoreCase("v")) {
-                // calculate V
+                calculateV();
+                running = false;
             } else if (selectedVariable.equalsIgnoreCase("n")) {
-                // calculate n
+                calculateN();
+                running = false;
             } else if (selectedVariable.equalsIgnoreCase("t")) {
-                // calculate T
+                calculateT();
+                running = false;
             } else {
                 System.out.println("Wrong input");
             }
         }
+        System.out.println("Enter anything to exit...");
+        input.nextLine();
     }
 
+    /**
+     * This method causes a loop which ends when an input can be parsed to float
+     * @return parsed float value
+     */
     private static float parseFloatFixed() {
         boolean running = true;
         float out = 0;
@@ -56,4 +66,60 @@ public class Main {
         return out;
     }
 
+    /* Simple methods for calculating variables */
+    private static void calculateP() {
+        System.out.println("Enter V value [m^3]");
+        V = parseFloatFixed();
+        System.out.println("Enter n value [mol]");
+        n = parseFloatFixed();
+        System.out.println("Enter T value [K]");
+        T = parseFloatFixed();
+
+        /* processing */
+        P = (n * R * T) / V;
+        System.out.println("P = " + P + " Pa \n"
+                + "for: V = " + V + " m^3 ; n = " + n + " mol ; T = " + T + " K");
+    }
+
+    private static void calculateV() {
+        System.out.println("Enter P value [Pa]");
+        P = parseFloatFixed();
+        System.out.println("Enter n value [mol]");
+        n = parseFloatFixed();
+        System.out.println("Enter T value [K]");
+        T = parseFloatFixed();
+
+        /* processing */
+        V = (n * R * T) / P;
+        System.out.println("V = " + V + " m^3 \n"
+                + "for: P = " + P + " Pa ; n = " + n + " mol ; T = " + T + " K");
+    }
+
+    private static void calculateN() {
+        System.out.println("Enter V value [m^3]");
+        V = parseFloatFixed();
+        System.out.println("Enter P value [Pa]");
+        P = parseFloatFixed();
+        System.out.println("Enter T value [K]");
+        T = parseFloatFixed();
+
+        /* processing */
+        n = (P * V) / R * T;
+        System.out.println("n = " + n + " mol \n"
+                + "for: V = " + V + " m^3 ; P = " + P + " Pa ; T = " + T + " K");
+    }
+
+    private static void calculateT() {
+        System.out.println("Enter V value [m^3]");
+        V = parseFloatFixed();
+        System.out.println("Enter P value [Pa]");
+        P = parseFloatFixed();
+        System.out.println("Enter n value [mol]");
+        n = parseFloatFixed();
+
+        /* processing */
+        T = (P * V) / R * n;
+        System.out.println("T = " + T + " K \n"
+                + "for: V = " + V + " m^3 ; P = " + P + " Pa ; T = " + n + " mol");
+    }
 }
